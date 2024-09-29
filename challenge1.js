8ab784ef2b0389685aab2bd3295521ce80b50ec5
Ashley Natasha <ashleyotsiula@gmail.com>
2024-09-29 22:05:34 +0300

    still coding

--- challenge1.js
+++ challenge1.js
@@ -1,14 +1,20 @@
 let grade = 86
 
+// A grade less than 40 should be Grade E
 if(grade>=0 && grade<=39) {console.log("Grade E")}
 
+// A grade between 40 and 49 should be Grade D
 else if(grade>=40 && grade<=49) {console.log("Grade D")}
 
+// A grade between 50 and 59 should be Grade C
 else if(grade>=50 && grade<=59) {console.log("Grade C")}
 
+// A grade between 60 and 79 should be Grade B
 else if(grade>=60 && grade<=79) {console.log("Grade B")}
 
+// A grade between 80 and 100 should be Grade A
 else if(grade>=80 && grade<=100) {console.log("Grade A")}
 
+// If a grade does not range from 0 to 100, the output should be "Invalid Output"
 else(console.log("Invalid Input"))
 
--- challenge2.js
+++ challenge2.js
@@ -1,11 +1,13 @@
 let speed = 120
 
+// If the speed if less than 70, the output should be "Ok".
 if(speed<=70) {console.log("Ok")}
 
+//If the speed is between 70 and 130, for every 5km/s above the speed limit(70), one demerit point is given and the total number is printed out.
 else if(speed>70 && speed<=130) {
     limit = speed - 70
     point = Math.floor(limit/5)
     console.log("Points:" + point);
 }
-
-else if(speed>130){console.log("License suspended.")}
+// If the speed is above 130, the function should print out "License suspended".
+else if(speed>130){console.log("License suspended")}
